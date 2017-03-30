40e522c89bfdd3b9ab31f6299fbb8cdc1f9ff5ed
orenk <oren.kabala@sirinmobile.com>
2017-03-31 00:25:56 +0300

    [urls.js] use Accuweather API

--- src/store.js
+++ src/store.js
@@ -10,9 +10,16 @@ export const store = new Vuex.Store({
 		showMenu: false,
 		posts: [],
         countries: [
-            {name: "Brazil", image: "https://www.w3schools.com/w3images/girl_train.jpg"},
-            {name: "Mexico", image: "https://www.w3schools.com/w3images/girl.jpg"},
-            {name: "Israel", image: "https://www.w3schools.com/w3images/natureboy.jpg"}
+					{name:"argentina", upper: "Argentina", id:"9562"},
+		{name:"belize", upper: "Belize", id:"27206"},
+		{name: "bolivia", upper: "Bolivia", id:"36072"},
+		{name:"brazil", upper:"Brazil", id:"2310390"},
+		{name:"chile", upper:"Chile", id:"60151"},
+		{name:"colombia", upper:"Colombia", id:"108205"},
+		{name:"mexico", upper:"Mexico", id:"236698"},
+		{name:"ecuador", upper:"Ecuador", id:"124692"},
+		{name:"peru", upper:"Peru", id:"258245"}
+
         ]
 	},
 	mutations: {
