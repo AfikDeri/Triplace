749e690a5ac4af8e93f7b49749e99c6ad284bce4
Afik Deri <deri.afik@gmail.com>
2017-03-30 15:27:02 +0300

    New app menu and content area in home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,43 @@
 <template>
-  <div>
-    
+  <div class="w3-main w3-content" style="max-width:1600px;margin-top:83px">
+  
+    <!-- Photo grid -->
+    <div class="w3-row w3-grayscale-min">
+      <div class="w3-quarter">
+        <img v-for="country in countries" :src="country.image" style="width:100%" @click="showCountry('brazil')" alt="Canoeing again">
+      </div>
+      <div class="w3-quarter">
+        <img v-for="country in countries" :src="country.image" style="width:100%" @click="showCountry('brazil')" alt="Canoeing again">
+      </div>
+      <div class="w3-quarter">
+        <img v-for="country in countries" :src="country.image" style="width:100%" @click="showCountry('brazil')" alt="Canoeing again">
+      </div>
+      <div class="w3-quarter">
+        <img v-for="country in countries" :src="country.image" style="width:100%" @click="showCountry('brazil')" alt="Canoeing again">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'home',
+
   data () {
     return {
       msg: 'Welcome to my awesome Vue.js App'
+    }
+  },
+
+  computed: {
+    countries(){
+      return this.$store.state.countries;
+    }
+  },
+
+  methods: {
+    showCountry(countryName){
+      console.log(countryName);
     }
   }
 }
@@ -17,21 +45,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
--- src/store.js
+++ src/store.js
@@ -7,7 +7,12 @@ export const store = new Vuex.Store({
 
 	state: {
 		user: null,
-		posts: []
+		posts: [],
+        countries: [
+            {name: "Brazil", image: "https://www.w3schools.com/w3images/girl_train.jpg"},
+            {name: "Mexico", image: "https://www.w3schools.com/w3images/girl.jpg"},
+            {name: "Israel", image: "https://www.w3schools.com/w3images/natureboy.jpg"}
+        ]
 	},
 	mutations: {
         setUser(state, payload){
