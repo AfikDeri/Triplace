9465cb26f4e8fb3b41ce940d832245f1ee745b27
orenk <oren.kabala@sirinmobile.com>
2017-03-30 20:39:47 +0300

    Fix urls.js

--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -11,6 +11,12 @@ export const GET_EXTENSIVE_FLIGHTS = "flights/extensive-search?apikey="; //Singl
 export const GET_LOW_FAIR_FLIGHTS = "flights/low-fare-search?apikey="; //Single traveler or affiliate
 export const GET_AFFILIATE_FLIGHTS = "flights/affiliate-search?apikey= &origin=LON&destination=DUB&departure_date=2017-08-25"; //Single traveler or affiliate
 
+export const GET_WEATHER_TO_7_DAYS_URL = "https://twcservice.mybluemix.net/api/weather/v1/geocode/"
+
+function buildWeatherTo7DaysURL(longitude , latitude) {
+  return GET_WEATHER_TO_7_DAYS_URL + latitude + "/" + longitude + "/forecast/daily/7day.json";
+}
+
 function urlencode(str) {
 str = escape(str);
 str = str.replace('+', '%2B');
@@ -27,46 +33,46 @@ str = unescape(str);
 return str;
 }
 
-function buildHotelURL(var latitude, var longitude, var radius, var check_in, var check_out)
+function buildHotelURL(latitude, longitude, radius, check_in, check_out)
 {
   return AMADEUS_URL_BASE + GET_HOTELS_URL + AMADEUS_API_KEY + "&latitude=" + latitude + "&longitude=" + longitude + "&radius=" + radius
                       + "&check_in=" + check_in + "&check_out=" + check_out;
 }
 
-function buildNearestRelevantAirportURL(var latitude, var longitude)
+function buildNearestRelevantAirportURL(latitude, longitude)
 {
   return AMADEUS_URL_BASE + NEAREST_RELEVANT_AIRPORT + AMADEUS_API_KEY + "&latitude=" + latitude + "&longitude=" + longitude;
 }
 
-function buildAirportAutocompleteURL(var airportCode)
+function buildAirportAutocompleteURL(airportCode)
 {
   return AMADEUS_URL_BASE + AIRPORT_AUTOCOMPLETE + AMADEUS_API_KEY + "&term=" + airportCode;
 }
 
-function buildLocationInformationURL(var IATACode)
+function buildLocationInformationURL(IATACode)
 {
   return AMADEUS_URL_BASE + LOCATION_INFORMATION + IATACode + "PAR?apikey=" + AMADEUS_API_KEY;
 }
 
-function buildGetInspirationFlightsURL(var origin, var destination, var departure_date, var duration)
+function buildGetInspirationFlightsURL(origin, destination, departure_date, duration)
 {
   return AMADEUS_URL_BASE + GET_INSPIRATION_FLIGHTS + AMADEUS_API_KEY + "&origin=" + origin + "&destination=" + destination
                         + "&departure_date=" + departure_date + "&duration=" + duration;
 }
 
-function buildGetExtensiveFlightsURL(var origin, var destination, var departure_date, var duration)
+function buildGetExtensiveFlightsURL(origin, destination, departure_date, duration)
 {
   return AMADEUS_URL_BASE + GET_EXTENSIVE_FLIGHTS + AMADEUS_API_KEY + "&origin=" + origin + "&destination=" + destination
                         + "&departure_date=" + departure_date + "&duration=" + duration;
 }
 
-function buildGetLowFairFlightsURL(var origin, var destination, var departure_date, var duration)
+function buildGetLowFairFlightsURL(origin, destination, departure_date, duration)
 {
   return AMADEUS_URL_BASE + GET_LOW_FAIR_FLIGHTS + AMADEUS_API_KEY + "&origin=" + origin + "&destination=" + destination
                         + "&departure_date=" + departure_date + "&duration=" + duration;
 }
 
-function buildGetInspirationFlightsURL(var origin, var destination, var departure_date, var duration)
+function buildGetInspirationFlightsURL(origin, destination, departure_date, duration)
 {
   return AMADEUS_URL_BASE + GET_INSPIRATION_FLIGHTS + AMADEUS_API_KEY + "&origin=" + origin + "&destination=" + destination
                         + "&departure_date=" + departure_date + "&duration=" + duration;
